dcf8120b12d1f33a02373ac91567b8cb0156fe1c
arthurvanl <arthurvanleeuwen@hotmail.nl>
2021-10-04 14:52:45 +0200

    Added events

--- test-socket/site/js/main.js
+++ test-socket/site/js/main.js
@@ -37,14 +37,30 @@ document.body.onkeyup = function (e) { //event
         hit.remove();
         const hits = hit.hits
         socket.emit('update', {userHits: hits, hit: -1}); //Stuur event naar de server.
+        console.log(hits)
+    }
+
+    if(e.keyCode == 82) { //r key code.
+        socket.emit('reset-counter', socket.id); //Stuur event naar de server.
     }
 
 }
 
+const spanElement = document.querySelector('#reset-count');
+
 socket.on('setCounter', function (e){ //Server event. Deze wordt aangeroepen zodra hij connect met de server
-    hit.hits = e;
+    hit.hits = e.totalHits;
     hit.update(); //Hit element updaten
-})
+    if(e.setSize > 0){
+        spanElement.innerHTML = `${e.setSize}/${e.userSize} willden de counter resetten.`;
+    } else {
+        spanElement.innerHTML = "";
+    }
+});
+
+socket.on('resetCount', function (e){ //resetCount event
+    spanElement.innerHTML = `${e.resetUsers}/${e.totalUsers} willen de counter resetten.`;
+});
 
 socket.on('updateCounter', function(e){ //Server event. Dit event wordt aangroepen zodra iemand de hit counter update.
     if(e.hit == 1){
@@ -54,4 +70,4 @@ socket.on('updateCounter', function(e){ //Server event. Dit event wordt aangroep
         hit.remove();
         console.log(e)
     } 
-})
+});
